fd3badc3be11fee21cd56f6101c33a66e77a9c11
DavidSousa <davidbdsousa@gmail.com>
2019-02-15 09:58:53 +0000

    changes to iOS grant permission

--- src/ios/FirebasePlugin.m
+++ src/ios/FirebasePlugin.m
@@ -82,27 +82,28 @@ static FirebasePlugin *firebasePlugin;
 
 - (void)grantPermission:(CDVInvokedUrlCommand *)command {
   if ([UNUserNotificationCenter class] != nil) {
-  // iOS 10 or later
-  // For iOS 10 display notification (sent via APNS)
-  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
-  UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
-  [[UNUserNotificationCenter currentNotificationCenter]
-      requestAuthorizationWithOptions:authOptions
-      completionHandler:^(BOOL granted, NSError * _Nullable error) {
-        // ...
-        // [[FIRMessaging messaging] setDelegate:self]; ???
-      }];
+    // iOS 10 or later
+    // For iOS 10 display notification (sent via APNS)
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert | UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+    [[UNUserNotificationCenter currentNotificationCenter]
+        requestAuthorizationWithOptions:authOptions
+        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+          // ...
+          // [[FIRMessaging messaging] setDelegate:self]; ???
+          [[UIApplication sharedApplication] registerForRemoteNotifications];
+          CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus: granted ? CDVCommandStatus_OK : CDVCommandStatus_ERROR];
+          [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+        }];
   } else {
     // iOS 10 notifications aren't available; fall back to iOS 8-9 notifications.
     UIUserNotificationType allNotificationTypes = (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
     UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
     [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus: CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
   }
-
-  [[UIApplication sharedApplication] registerForRemoteNotifications];
-
-  CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-	[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 	return;
 }
 
