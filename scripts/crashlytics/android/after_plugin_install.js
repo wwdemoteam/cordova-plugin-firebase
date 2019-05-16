15bd7ce944f35735b924227fe84ab161fc497d57
DavidSousa <davidbdsousa@gmail.com>
2019-05-16 23:15:41 +0100

    update Cordova 9

--- scripts/crashlytics/android/after_plugin_install.js
+++ scripts/crashlytics/android/after_plugin_install.js
@@ -5,5 +5,5 @@ module.exports = function(context) {
     // Modify the Gradle build file to add a task that will upload the debug symbols
     // at build time.
     helper.restoreRootBuildGradle();
-    helper.modifyRootBuildGradle();
+    helper.modifyRootBuildGradle(context);
 };
